200e41ab68b1c4fa19f65d452faf8cffd19dc7b7
upiksaleh <upik@codeup.id>
2026-04-23 10:12:43 +0700

    terraform module cloudflare_zone

---
+++ terraform/cloudflare/zone/main.tf
@@ -0,0 +1,13 @@
+resource "cloudflare_zone" "this" {
+  name                = var.zone_name
+  paused              = false
+  type                = "full"
+  vanity_name_servers = []
+  account = {
+    id = var.account_id
+  }
+}
+
+output "id" {
+  value = cloudflare_zone.this.id
+}
---
+++ terraform/cloudflare/zone/variables.tf
@@ -0,0 +1,9 @@
+variable "account_id" {
+  type        = string
+  description = "Cloudflare Account ID"
+}
+
+variable "zone_name" {
+  type        = string
+  description = "Cloudflare Zone Name (e.g., example.com)"
+}
